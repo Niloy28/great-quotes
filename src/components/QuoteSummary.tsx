39a0ad30f21384e929120df409e8dbb5f3080fa8
Farhan Ishrak Islam <niloy28@live.com>
2023-04-24 14:41:51 +0600

    Added transition animations.

--- src/components/QuoteSummary.tsx
+++ src/components/QuoteSummary.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Quote from "../types/Quote";
 import Container from "./UI/Container";
+import { NavLink } from "react-router-dom";
 
 const QuoteSummary: React.FC<Quote> = (props) => {
 	return (
-		<Container className="border-2 md:border-4 flex flex-col gap-2 justify-between my-2 md:my-4">
-			<div className="flex justify-between">
-				<div className="font-semibold text-lg md:text-xl">{props.title}</div>
-				<div className="italic">
-					{new Date(props.created_at).toLocaleDateString(undefined, {
-						weekday: "short",
-						year: "numeric",
-						month: "long",
-						day: "numeric",
-					})}
+		<NavLink to={`/quotes/${props.id}`}>
+			<Container className="border-2 md:border-4 flex flex-col gap-2 justify-between my-2 md:my-4 cursor-pointer transition ease-in-out delay-0 hover:scale-105 duration-200">
+				<div className="flex justify-between">
+					<div className="font-semibold text-lg md:text-xl">{props.title}</div>
+					<div className="italic">
+						{new Date(props.created_at).toLocaleDateString(undefined, {
+							weekday: "short",
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						})}
+					</div>
 				</div>
-			</div>
-			<p className="font-bold text-xl md:text-2xl italic p-2">{props.quote}</p>
-		</Container>
+				<p className="font-bold text-xl md:text-2xl italic p-2">
+					{props.quote}
+				</p>
+			</Container>
+		</NavLink>
 	);
 };
 
