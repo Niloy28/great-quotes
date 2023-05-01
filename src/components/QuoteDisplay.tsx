7ce31721fbdd8626e9eff285881156b6913108c9
Farhan Ishrak Islam <niloy28@live.com>
2023-05-01 14:45:40 +0600

    Fix layout.

--- src/components/QuoteDisplay.tsx
+++ src/components/QuoteDisplay.tsx
@@ -5,9 +5,11 @@ import Heading from "./UI/Heading";
 
 const QuoteDisplay: React.FC<Quote> = (props) => {
 	return (
-		<Container className="border-lime-100 border-2 my-10">
-			<Heading className="italic -translate-x-1/4">{props.quote}</Heading>
-			<div className="translate-x-2/3 md:translate-x-3/4 text-lg md:text-2xl italic">
+		<Container className="border-lime-100 border-2 my-10 flex flex-col">
+			<Heading className="pl-4 md:pl-8 italic self-start justify-self-start">
+				{props.quote}
+			</Heading>
+			<div className="self-end justify-self-end text-lg md:text-2xl italic pr-4 md:pr-8">
 				~{props.author}
 			</div>
 		</Container>
